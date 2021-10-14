8b975f1bc13ed1257827237a6b3a8e23dfa41a15
Olga Tsylko <helgatsylko@gmail.com>
2021-10-14 19:56:03 +0300

    Homework 9

--- main.js
+++ main.js
@@ -5,19 +5,19 @@ let direct2 = './testDir2';
 let file1 = './testDir1/testFile.txt';
 let file2 = './testDir2/testFile.txt';
 
-function foo2 (action, callback){
+function foo (action, callback){
   setTimeout (() => {
     if (callback) callback (action)
   }, 2000)
 }
 
-foo2 (dir.crDir(direct1), () => {
-  foo2 (dir.crFile(file1), () => {
-    foo2 (dir.crDir(direct2), () => {
-      foo2 (dir.moveFile(file1, file2), () => {
-        foo2 (dir.remFile(file2), () => {
-          foo2 (dir.remDir(direct1), () => { 
-            foo2 (dir.remDir(direct2)); 
+foo (dir.crDir(direct1), () => {
+  foo (dir.crFile(file1), () => {
+    foo (dir.crDir(direct2), () => {
+      foo (dir.moveFile(file1, file2), () => {
+        foo (dir.remFile(file2), () => {
+          foo (dir.remDir(direct1), () => { 
+            foo (dir.remDir(direct2)); 
           })
         })
       })
